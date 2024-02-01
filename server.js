9900b604d044437bb428e8904a327dac80c59053
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-02-01 14:32:44 +0800

    added log in view and fix routing

--- server.js
+++ server.js
@@ -52,13 +52,12 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// account routes
+app.use("/account", require("./routes/accountRoute")) 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
-// account routes
-app.use("/account", require("./routes/accountRoute")) 
-
 
 /* ***********************
 * Express Error Handler
