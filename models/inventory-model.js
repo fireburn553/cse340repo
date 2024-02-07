0f17139e930ee6ab84434c0c0fd71e50e6ec8136
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-02-07 20:10:05 +0800

    added delete functionality

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -105,5 +105,16 @@ async function updateInventory(
     console.error("model error: " + error)
   }
 }
-
-module.exports = {getClassifications, getInventoryByClassificationId, getDetailsByInventoryId, addClassification, checkExistingClassification, addInventory, updateInventory}
+/* ***************************
+ *  Delete Inventory Item
+ * ************************** */
+async function deleteInventory(inv_id) {
+  try {
+    const sql = 'DELETE FROM inventory WHERE inv_id = $1'
+    const data = await pool.query(sql, [inv_id])
+  return data
+  } catch (error) {
+    new Error("Delete Inventory Error")
+  }
+}
+module.exports = {getClassifications, getInventoryByClassificationId, getDetailsByInventoryId, addClassification, checkExistingClassification, addInventory, updateInventory, deleteInventory}
